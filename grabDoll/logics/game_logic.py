54a9167dcb04ef35b8019217c71e2fb57d9566a2
May25th <214816701@qq.com>
2017-09-29 16:24:57 +0800

    ceshi

--- grabDoll/logics/game_logic.py
+++ grabDoll/logics/game_logic.py
@@ -24,5 +24,7 @@ def get_game_info(uid, open_key):
         'friend': friend_logic.get_my_friend_info(uid),
     }
 
+if __name__ == "__main__":
+    print get_game_info('ED57884CAA078DF9E0E08750D98CA834', 'F7D770DA0E6E8BDC6FF1D0A652925E2B')
 
 
