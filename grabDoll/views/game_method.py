d3ef1290a167ed93f82bfe968965ed5b0992fd95
May25th <214816701@qq.com>
2017-11-03 17:11:54 +0800

    test

--- grabDoll/views/game_method.py
+++ grabDoll/views/game_method.py
@@ -14,9 +14,9 @@ __author__ = 'du_du'
 def grab_egg(request):
     if request.method == "POST":
         try:
-            print(request.query_params)
-            uid = request.query_params.get('uid')
-            item_id = request.query_params.get('item_id')
+            print(request.data)
+            uid = request.data['uid']
+            item_id = request.data['item_id']
         except Exception as e:
             print(e)
             return 1, "参数错误"
@@ -28,10 +28,10 @@ def grab_egg(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def switch_machine(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             print(request.query_params)
             uid = request.query_params.get('uid')
@@ -47,10 +47,10 @@ def switch_machine(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def reset_machine(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             print(request.query_params)
             uid = request.query_params.get('uid')
@@ -65,10 +65,10 @@ def reset_machine(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def buy_shop(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             uid = request.query_params.get('uid')
             shop_id = request.query_params.get('shop_id')
@@ -85,10 +85,10 @@ def buy_shop(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def use_item(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             uid = request.query_params.get('uid')
             item_id = request.query_params.get('item_id')
@@ -105,10 +105,10 @@ def use_item(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def speed_up(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             uid = request.query_params.get('uid')
             item_id = request.query_params.get('item_id')
@@ -125,10 +125,10 @@ def speed_up(request):
         return 1, "数据错误"
 
 
-@api_view(["POST"])
+@api_view(["GET"])
 @api_result
 def get_debug(request):
-    if request.method == "POST":
+    if request.method == "GET":
         try:
             debug_info = str(request.query_params.get('debug'))
         except Exception as e:
