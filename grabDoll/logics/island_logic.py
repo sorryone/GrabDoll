d6fedfe7f00e421397cebe4ac734f7ecb9aceb4a
May25th <214816701@qq.com>
2017-11-29 14:37:04 +0800

    Fight

--- grabDoll/logics/island_logic.py
+++ grabDoll/logics/island_logic.py
@@ -9,18 +9,21 @@ __author__ = 'du_du'
 def refresh_income_info(uid):
     action = FormationAction(uid)
     info = action.get_model_info()
+    print(info)
     capacity = info.get('capacity', 0)
     capacity_update_at = info.get('capacity_update_at', 0)
     income = info.get('income', 0)
     cur_time = int(time.time())
     # 每个小时的获取的金币
     per_hour_capacity = capacity / 10
+    print(capacity, income, per_hour_capacity)
     income = int((cur_time - capacity_update_at) / 3600 * per_hour_capacity) + income
     res = {
         'income': income,
         'capacity_update_at': cur_time,
     }
     if action.set_values(res):
+        print(res)
         return res
     return False
 
