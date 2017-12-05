ef7fdb98a9a10ee57b9d0c84b0b8a87c120059c7
May25th <214816701@qq.com>
2017-12-05 16:42:58 +0800

    Article

--- grabDoll/logics/artifact_logic.py
+++ grabDoll/logics/artifact_logic.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from grabDoll.models.config_model import ConfigModel
-from grabDoll.action.book_action import HandBookAction
-from grabDoll.action.hero_action import HeroAction
+from grabDoll.action.artifact_action import ArtifactAction
 __author__ = 'du_du'
 
 
 # 抓完娃娃后刷新当前的图鉴
 def get_artifact_info(uid):
-    config_model = ConfigModel('artifact')
-    return False
+    # config_model = ConfigModel('artifact')
+    action = ArtifactAction(uid)
+    return action.get_model_info()
 
 
 # 判定机器是否已经解锁
