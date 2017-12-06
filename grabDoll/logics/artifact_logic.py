ca0344eb42c9068063da0f7a7f546cc8c1f41907
May25th <214816701@qq.com>
2017-12-06 18:25:09 +0800

    Article

--- grabDoll/logics/artifact_logic.py
+++ grabDoll/logics/artifact_logic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from grabDoll.models.config_model import ConfigModel
 from grabDoll.action.artifact_action import ArtifactAction
+from grabDoll.action.item_action import  ItemAction
 __author__ = 'du_du'
 
 
@@ -18,10 +19,26 @@ def upgrade_artifact(uid, artifact):
     if artifact_config is False:
         print('流氓数据 错误的ID')
         return False
+    check_data = action.get_model_info_by_id(artifact)
+    if check_data is None or check_data is False or len(check_data):
+        print('流氓数据 骗子')
+        return False
     next_id = artifact_config.get('next_id')
     if next_id == -1:
         print('流氓数据 满级了')
         return False
+    upgrade_item = check_data.get('upgrade')
+    upgrade_items = eval(upgrade_item)
+    item_model = ItemAction(uid)
+    all_item_info = item_model.get_model_info()
+    for a_id, ct in upgrade_items.iteritems():
+        if int(all_item_info.get(a_id)) < ct:
+            print('消耗物品不够', a_id)
+            return False
+    for a_id, ct in upgrade_items.iteritems():
+        remove_res = item_model.remove_model(a_id, ct)
+        if remove_res is False:
+            print (uid, '删除失败', a_id, ct)
     return action.replace_model(artifact, next_id)
 
 
