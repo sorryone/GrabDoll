9717358198c6c7c2e8f75ab4f174aab1d49909bb
May25th <214816701@qq.com>
2017-12-11 11:54:19 +0800

    Pve

---
+++ grabDoll/models/pve_model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from lib.redis_model import HashModel
+from rest_framework import serializers
+from django.db import models
+from grabDoll.common.serializerutils import UnixEpochDateField
+__author__ = 'du_du'
+
+
+class PveModel(HashModel):
+    pass
+
+
+class PveTable(models.Model):
+    u_id = models.CharField(max_length=32, unique=True)
+    pve_id = models.IntegerField(default=0)
+    boss_hp = models.IntegerField(default=0)
+    fight_refresh_time = models.IntegerField(default=0)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modify_at = models.DateTimeField(auto_now=True)
+
+
+class PveTableSerializer(serializers.ModelSerializer):
+    create_at = UnixEpochDateField(required=False, allow_null=True)
+    modify_at = UnixEpochDateField(required=False, allow_null=True)
+
+    class Meta:
+        model = PveTable
+        fields = '__all__'
