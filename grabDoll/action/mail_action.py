130359b400ac5b2d297d7c01e2d5899a81fbf93e
May25th <214816701@qq.com>
2017-12-19 14:21:32 +0800

    Mail

--- grabDoll/action/mail_action.py
+++ grabDoll/action/mail_action.py
@@ -16,20 +16,39 @@ class MailAction(BaseModel):
         self.mType = 'mType'
         self.info_str = 'info'
         self.award_str = 'award'
+        self.read_str = 'read'
+        self.create_at_str = 'create_at'
         self.lv_up_type = 1
         self.invite_type = 2
         self.friend_type = 3
+        self.key_info = ('u_id', self.key_str, self.fr_id_str, self.mType, self.info_str, self.award_str, self.create_at_str, self.read_str)
         super(MailAction, self).__init__(
                     u_id, MailModel, MailTable, MailTableSerializer, True)
 
     def get_model_info(self):
         data_list = self.get_all()
-        res = sorted(data_list, key=lambda x: x['create_at'], reverse=False)
+        res = []
+        if isinstance(data_list, (dict, collections.OrderedDict)):
+            res_item = self.filter_data(data_list)
+            res.append(res_item)
+        else:
+            # 如果是数组
+            for index, data in enumerate(data_list):
+                res_item = self.filter_data(data)
+                res.append(res_item)
+        res = sorted(data_list, key=lambda x: x[self.create_at_str], reverse=False)
         return res
 
     def get_model_info_by_id(self, w_id):
         data = self.get_all({self.key_str: w_id})
         return data
+
+    def filter_data(self, data):
+        res_item = dict()
+        for key in self.key_info:
+            if key in data:
+                res_item[key] = data[key]
+        return res_item
 
     # 升级奖励
     def add_lv_up_award(self, award):
