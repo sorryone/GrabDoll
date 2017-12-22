a53e7b38e3e043115f26c5053bd404606fd3d00b
May25th <214816701@qq.com>
2017-12-22 13:43:21 +0800

    turntable

--- grabDoll/logics/turntable_logic.py
+++ grabDoll/logics/turntable_logic.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from grabDoll.logics import inventory_logic
 from grabDoll.models.config_model import ConfigModel
+from grabDoll.action.user_action import UserAction
 import random
 __author__ = 'du_du'
 
 
 # 抽取一次
 def try_once(uid):
+    gold_cost = 100
+    u_action = UserAction(uid)
+    if u_action.reduce_gold(gold_cost) is False:
+        return False
     turntable_config_model = ConfigModel('turntable')
     turntable_config_info = turntable_config_model.get_model_info()
     random_list = []
@@ -14,9 +19,12 @@ def try_once(uid):
         random_list.extend([key] * value['chance'])
     selected_key = random.choice(random_list)
     print(turntable_config_info[selected_key])
-    return True
-    award = []
-    return inventory_logic.add_awards(uid, award)
+    award = {turntable_config_info['item_id']: turntable_config_info['ct']}
+    res = {
+        'selected': turntable_config_info['config_id'],
+        'award': inventory_logic.add_awards(uid, award),
+    }
+    return res
 
 
 # 抽取一次
