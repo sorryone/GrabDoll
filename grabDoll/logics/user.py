39d3d2c9556f185888611f9ebddc3ae74b51f685
May25th <214816701@qq.com>
2017-12-26 13:29:41 +0800

    turntable

--- grabDoll/logics/user.py
+++ grabDoll/logics/user.py
@@ -42,7 +42,6 @@ def add_exp(uid, add_value):
         data[u.exp_str] = latest_exp - cur_lv_config.get('exp', 0)
     else:
         data[u.exp_str] = latest_exp
-
     return u.update_info(data)
 
 
