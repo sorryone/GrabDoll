209ee77efd3a04321bbfb5ea2d2363ff25ad4f72
May25th <214816701@qq.com>
2018-01-03 14:41:59 +0800

    test

--- grabDoll/models/config_model.py
+++ grabDoll/models/config_model.py
@@ -15,7 +15,7 @@ class ConfigModel(HashModel):
     def get_config_by_id(self, config_id):
         type_value = int(int(config_id)/10000)
         # 先判定抓到的娃娃蛋存不存在
-        types = ("null", 'egg', 'item', 'artifact', 'doll', 'machine', 'shop', 'doll_upgrade', 'pve', 'turntable', 'user_lv')
+        types = ("null", 'egg', 'item', 'artifact', 'doll', 'machine', 'shop', 'doll_upgrade', 'pve', 'turntable', 'task', 'user_lv')
         if len(types) >= type_value:
             res = self.get_value(config_id)
             if res:
