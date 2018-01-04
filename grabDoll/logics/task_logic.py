cb75cc0586718efdc1df41c015e04d4fe33d7ed3
May25th <214816701@qq.com>
2018-01-04 11:29:28 +0800

    test

--- grabDoll/logics/task_logic.py
+++ grabDoll/logics/task_logic.py
@@ -17,5 +17,5 @@ def update_task_info(uid, action_type, task_target, task_value):
     config_model = ConfigModel('task')
     all_task_config_group = config_model.get_model_info()
 
-    check_group = [x for x in all_task_config_group if x.get('action_type', '') == action_type]
+    check_group = [x for x in all_task_config_group.values() if x.get('action_type', '') == action_type]
     return check_group
