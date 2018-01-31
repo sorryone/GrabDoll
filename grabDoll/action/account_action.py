66c95a85e727e65ff71288474c8e22d5c5d059f7
May25th <214816701@qq.com>
2018-01-31 15:14:55 +0800

    QQ

--- grabDoll/action/account_action.py
+++ grabDoll/action/account_action.py
@@ -27,12 +27,12 @@ class AccountAction(BaseModel):
         return data
 
     def create_model(self, open_id, platform):
-        system_id = uuid.uuid3(uuid.NAMESPACE_DNS, open_id + str(platform)).int
+        system_id = uuid.uuid3(uuid.NAMESPACE_DNS, str(open_id) + str(platform)).int
         last_id = self.get_last_id()
         head_id = max(1, last_id/self.head_rate)
         str_id_len = max(self.account_ran_len - len(str(last_id)), 1)
         str_id = str(system_id)[0:str_id_len]
-        game_u_id = head_id + self.get_last_id() + str_id
+        game_u_id = '%d%d%d%s' % (head_id, platform, self.get_last_id(), str_id)
         self.u_id = game_u_id
         return self.u_id
         if self.set_values({self.open_id_str: open_id, self.platform_str: platform}):
