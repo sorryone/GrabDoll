1f9643fbd5d6a7ab76a492446f267ab4a97c89e4
May25th <214816701@qq.com>
2018-01-31 15:41:31 +0800

    QQ

--- grabDoll/action/account_action.py
+++ grabDoll/action/account_action.py
@@ -24,7 +24,7 @@ class AccountAction(BaseModel):
 
     def get_model_info_by_id(self, open_id, platform):
         data = self.get_user_id_by_matching({self.open_id_str: open_id, self.platform_str: platform})
-        if data is False or len(data):
+        if data is False or len(data) == 0:
             return False
         return data
 
