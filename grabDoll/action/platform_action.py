2ad3c9773327d61cc6aa24ffb946c4f699fa6748
May25th <214816701@qq.com>
2018-02-08 18:07:43 +0800

    QQ

--- grabDoll/action/platform_action.py
+++ grabDoll/action/platform_action.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from grabDoll.models.base_model import BaseModel
 from grabDoll.models.platform_model import PlatformModel, PlatformTable, PlatformTableSerializer
+import time
 __author__ = 'du_du'
 
 
@@ -18,3 +19,8 @@ class PlatformAction(BaseModel):
     def get_private_info(self):
         data = self.get_all()
         return data
+
+    def test_model_info(self):
+        start_time = time.time()
+        data = self.get_all()
+        return time.time() - start_time
