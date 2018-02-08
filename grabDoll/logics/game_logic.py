d46bfd7d2bc3face9de4cb876c4e3fc78af2691b
May25th <214816701@qq.com>
2018-02-08 17:31:14 +0800

    QQ

--- grabDoll/logics/game_logic.py
+++ grabDoll/logics/game_logic.py
@@ -71,6 +71,77 @@ def get_user_data(open_id, open_key, platform):
     return res
 
 
+def get_user_test(open_id, open_key, platform):
+    start_time = time.time()
+    platform_info = platform_logic.get_user_info_by_platform(open_id, open_key, platform)
+    if platform_info is False:
+        return False
+    # 需要获得一个用户的游戏里的唯一ID
+    a_action = AccountAction('')
+    game_user_id = a_action.get_model_info_by_id(open_id, platform)
+    if game_user_id is False:
+        game_user_id = a_action.create_model(open_id, platform)
+    uid = game_user_id
+    time_str_1 = time.time()
+    # 当日首次登陆的检查
+    note_model = NoteModel(uid)
+    last_refresh_time = note_model.get_login_time()
+    cur_time = time.time()
+    today_zero_time = cur_time-cur_time % 86400 + time.timezone
+    if last_refresh_time < today_zero_time:
+        note_model.set_login_time()
+        # 刷新体力
+        user_logic.refresh_vit(uid)
+    # 刷新收入
+    island_logic.refresh_income_info(uid)
+
+    refresh_config = {
+        'income': 60,   # 收入刷新的间隔
+        'vit': 3600,    # 体力刷新的时间
+    }
+    r_action = RecordAction(uid)
+    r_info = r_action.get_model_info()
+    time_str_2 = time.time()
+    user_info = user_logic.get_user_info(uid)
+    inventory = inventory_logic.get_inventory_info(uid)
+    machine_info = machine_logic.get_machine_info(uid)
+    note_info = machine_logic.get_note_info(uid)
+    time_str_3 = time.time()
+    hatch_info = hatch_logic.get_hatch_info(uid)
+    book_info = machine_logic.get_book_info(uid)
+    ran_info = rank_logic.get_rank_info(uid, open_key)
+    time_str_4 = time.time()
+    formation_info = formation_logic.get_formation_info(uid)
+    artifact_info = artifact_logic.get_artifact_info(uid)
+    pve_info = pve_logic.get_pve_info(uid)
+    mail_info = mail_logic.get_mail_info(uid)
+    task_info = task_logic.get_task_info(uid)
+    res = {
+        'game_user_id': uid,
+        'user': platform_info,
+        'user_info': user_info,
+        'inventory': inventory,
+        'machine': machine_info,
+        'note': note_info,
+        'hatch': hatch_info,
+        'book': book_info,
+        'rank': ran_info,
+        'formation': formation_info,
+        'artifact': artifact_info,
+        'pve': pve_info,
+        'mail': mail_info,
+        'refresh': refresh_config,
+        'record': r_info,
+        'task': task_info,
+    }
+    print('step1', time_str_1 - start_time)
+    print ('step2', time_str_2 - time_str_1)
+    print ('step3', time_str_3 - time_str_2)
+    print ('step4', time_str_4 - time_str_3)
+    print ('get_user_data cost time', time.time() - start_time)
+    return res
+
+
 if __name__ == "__main__":
     # print get_game_info('ED57884CAA078DF9E0E08750D98CA834', 'F7D770DA0E6E8BDC6FF1D0A652925E2B')
     print get_user_data('VIP', 'VIP')
--- grabDoll/views/user.py
+++ grabDoll/views/user.py
@@ -26,3 +26,23 @@ def get_user_data(request):
         print(e)
         return 1, "数据错误"
 
+
+@api_view(["GET"])
+@api_result
+def get_user_test(request):
+    if request.method == "GET":
+        try:
+            print('get_user_data', 'user')
+            uid = request.query_params.get('openid').encode('utf-8')
+            open_key = request.query_params.get('openkey').encode('utf-8')
+            platform = int(request.query_params.get('platform'))
+        except Exception as e:
+            print(e)
+            return 1, "参数错误"
+    try:
+        data = game_logic.get_user_test(uid, open_key, platform)
+        return 0, data
+    except Exception as e:
+        print(e)
+        return 1, "数据错误"
+
